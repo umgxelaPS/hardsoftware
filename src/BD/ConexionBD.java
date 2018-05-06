b95334147a3df30a8b8eb907f667476c55a72ec0
Martin Ajucum <martin_aj22@hotmail.com>
2018-05-06 15:11:05 -0600

    Ignore v2

--- src/BD/ConexionBD.java
+++ src/BD/ConexionBD.java
@@ -34,7 +34,7 @@ public class ConexionBD extends Application {
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            conectar = DriverManager.getConnection("jdbc:mysql://25.75.85.180:3306/bd_hardsoftware","marlon","enrique");
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_hardsoftware","root","1490Martin");
 
         } catch (Exception e) {
             System.out.println(e);
