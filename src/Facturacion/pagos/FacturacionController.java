8e721e6b53fda5b09d6552bd54187fa704c2bbd9
marlon <marlonmfabian87@gmail.com>
2018-05-26 04:15:42 -0600

    Carga de producto a bd1

--- src/Facturacion/pagos/FacturacionController.java
+++ src/Facturacion/pagos/FacturacionController.java
@@ -5,11 +5,21 @@
  */
 package Facturacion.pagos;
 
+import BD.ConexionBD;
+import Empleados.RegistroEmpleadosController;
 import Facturacion.articulo;
+import Reportes.ClaseProducto;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,6 +34,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -47,11 +58,17 @@ public class FacturacionController implements Initializable {
 
     private double xOffset = 0;
     private double yOffset = 0;
-         //Variables para Cambio de escenas
+    //Variables para Cambio de escenas
     Node node;
     Stage stage;
     Parent parent;
     Scene root;
+    //Instancia Conexión BD
+    ConexionBD conectar = new ConexionBD();
+    Connection con = conectar.conexion();
+    PreparedStatement preparar;
+    ResultSet result;
+
     /**
      * Initializes the controller class.
      */
@@ -69,45 +86,68 @@ public class FacturacionController implements Initializable {
 
     @FXML
     private void confirmAction(ActionEvent event) throws IOException {
-      /*  if (validateInput()) {
-            double paid = Double.parseDouble(paidAmountField.getText().trim());
-            double retail = Math.abs(paid - netPrice);
+        try {
+            if (validateInput()) {
+                double paid = Double.parseDouble(paidAmountField.getText().trim());
+                double retail = Math.abs(paid - netPrice);
+                int nitCliente = 0, modopago = 0;
+                String cliente = "", fecha = "";
 
-            String invoiceId = String.valueOf(new Timestamp(System.currentTimeMillis()).getTime());
+                String invoiceId = String.valueOf(new Timestamp(System.currentTimeMillis()).getTime());
+                System.out.println("Factuacion controller: " + invoiceId);
+for (articulo i : items){
+    System.out.println("Item: "+i.getItemName());
+}
+                /* fact venta = new fact(
+                Integer.parseInt(invoiceId), nitCliente, modopago, cliente, fecha
+                );*/
+                //Sentencia SQL**************
+                String capturadol = "";
+                String capturado2 = "";
+                String sql = "SELECT MAX(no_factura) AS no_factura FROM factura";
 
-            factura invoice = new factura(
-                    invoiceId,
-                    employeeModel.getEmployee(2),
-                    payment.getSubTotal(),
-                    payment.getVat(),
-                    payment.getDiscount(),
-                    payment.getPayable(),
-                    paid,
-                    retail
-            );
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                while (rs.next()) {
+                    capturadol = rs.getString("no_factura");
 
-            invoiceModel.saveInvoice(invoice);
-            //*****Importantisimo************
-            /*      
-           for (articulo i : items) {
+                }
+                System.out.println("No factura"+capturadol);
+                //***************
+             /*/  String sql;
 
+                //Guardar Registros en BD
+                sql = "INSERT INTO bd_hardsoftware.factura (no_factura,nit_cliente,nombre_cliente,fecha_emision,modo_pago) VALUES (?,?,?,?,?);";
+
+                preparar = con.prepareStatement(sql);
+                preparar.setInt(1, Integer.parseInt(invoiceId));
+                preparar.setInt(2, nitCliente);
+                preparar.setInt(3, modopago);
+                preparar.setString(4, cliente);
+                preparar.setString(5, fecha);
+                preparar.executeUpdate();
+*/
+                //invoiceModel.saveInvoice(invoice);
+                //*****Importantisimo************
+
+                /* for (articulo i : items) {
+                
                 Producto p = productModel.getProductByName(i.getItemName());
                 double quantity = p.getQuantity() - i.getQuantity();
                 p.setQuantity(quantity);
                 productModel.decreaseProduct(p);
-
+                
                 Sale sale = new Sale(
-                        invoiceModel.getInvoice(invoiceId),
-                        productModel.getProductByName(i.getItemName()),
-                        i.getQuantity(),
-                        i.getUnitPrice(),
-                        i.getTotal()
+                invoiceModel.getInvoice(invoiceId),
+                productModel.getProductByName(i.getItemName()),
+                i.getQuantity(),
+                i.getUnitPrice(),
+                i.getTotal()
                 );
-
+                
                 salesModel.saveSale(sale);
-            }
-            try {
-                /* FXMLLoader loader = new FXMLLoader((getClass().getResource("/Facturacion/pagos/confirmar.fxml")));
+                }*/
+                FXMLLoader loader = new FXMLLoader((getClass().getResource("/Facturacion/pagos/confirmar.fxml")));
                 ConfirmarController controller = new ConfirmarController();
                 //Datos importante
                 //controller.setData(retail, items, invoiceId);
@@ -116,23 +156,21 @@ public class FacturacionController implements Initializable {
                 Scene scene = new Scene(root);
                 Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 root.setOnMousePressed((MouseEvent e) -> {
-                xOffset = e.getSceneX();
-                yOffset = e.getSceneY();
+                    xOffset = e.getSceneX();
+                    yOffset = e.getSceneY();
                 });
                 root.setOnMouseDragged((MouseEvent e) -> {
-                stage.setX(e.getScreenX() - xOffset);
-                stage.setY(e.getScreenY() - yOffset);
+                    stage.setX(e.getScreenX() - xOffset);
+                    stage.setY(e.getScreenY() - yOffset);
                 });
-                stage.setTitle("Confirm");
-                stage.getIcons().add(new Image("/Imagenes/logo.png"));
+                stage.setTitle("Confirmar");
+                //stage.getIcons().add(new Image("Imagenes/logo.png"));
                 stage.setScene(scene);
-                stage.show();*
-            //Variables para Cambio de escenas
-       
-            } catch (Exception e) {
-                System.out.println("Error en Facturacion Controller: " + e);
+                stage.show();
+                //Variables para Cambio de escenas
+
             }
-        }*/
+            /*
        node=(Node) event.getSource();
         stage=(Stage) node.getScene().getWindow();
         
@@ -143,6 +181,11 @@ public class FacturacionController implements Initializable {
         stage.centerOnScreen();
         stage.setTitle("Registro Empleados");
         stage.show();
+             */
+        } catch (Exception e) {
+            System.out.println("Error en Facturacion controller:" + e);
+        }
+
     }
 
     public void setData(double netPrice, ObservableList<articulo> items, pago payment) {
